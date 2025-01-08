6fe5b10bdadb17eaa3e112ee374c7c75dba325da
Muskelmax <maxbuhler50@gmail.com>
2025-01-08 15:02:58 +0100

    added data to model.py

---
+++ model.py
@@ -0,0 +1,23 @@
+departure = [
+  {
+    "name": "5C",
+    "direction" : "Husum Torv",
+    "scheduled": "10:17",
+    "actual" : "10:26",
+    "within" : "3 min"
+  },
+  {
+    "name": "250S",
+    "direction" : "Gladsaxe",
+    "scheduled": "10:17",
+    "actual" : "10:26",
+    "within" : "5 min"
+  },
+  {
+    "name": "15",
+    "direction" : "Hellerup",
+    "scheduled": "10:17",
+    "actual" : "10:26",
+    "within" : "12 min"
+  }
+]
