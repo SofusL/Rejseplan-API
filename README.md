5b1e3f47dacc143976e13d064be838958613c973
Muskelmax <maxbuhler50@gmail.com>
2025-01-14 14:03:53 +0100

    made refresh actually work

--- README.md
+++ README.md
@@ -4,3 +4,5 @@ A school project using a rejseplan API
 To run the application you have to add a .env file, where you append a valid rejseplanen-api-key to the API_KEY variable
 
 - [x] være sej
+
+- [ ] ikke være sej
--- view.py
+++ view.py
@@ -7,7 +7,7 @@ class UserView:
     self.root.geometry("1080x1920") 
     
   def refresh(self):
-    pass
+    self.run()
 
   def setController(self, controller):
     self.controller = controller
@@ -15,7 +15,7 @@ class UserView:
   def run(self):
     self.departure = self.controller.fetchDatas()
     print(self.departure)
-    self.refresh_Button = Button(self.root, text="✝️", bd = '5', command=self.refresh()).grid(row=3, column=5, padx=5, pady=5)
+    self.refresh_Button = Button(self.root, text="✝️", bd = '5', command=self.refresh).grid(row=3, column=5, padx=5, pady=5)
     Label(self.root, text="Bus", width=10, anchor="w").grid(row=1, column=0, padx=5, pady=2)
     Label(self.root, text="Destination", width=20, anchor="w").grid(row=1, column=1, padx=5, pady=2)
     Label(self.root, text="Planmæssig afgang", width=15, anchor="w").grid(row=1, column=2, padx=5, pady=2)
